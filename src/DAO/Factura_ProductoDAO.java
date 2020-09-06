5647a146473c9ec835adfa6caa61e4528517042c
EzeBalcaldi <42391879+EzeBalcaldi@users.noreply.github.com>
2020-09-06 18:07:21 -0300

    Update Factura_ProductoDAO.java

--- src/DAO/Factura_ProductoDAO.java
+++ src/DAO/Factura_ProductoDAO.java
@@ -14,7 +14,7 @@ import org.apache.commons.csv.CSVRecord;
 import conexion.Conexion;
 
 public class Factura_ProductoDAO {
-	private Conexion conexion;
+	private Conexion conexion; //Usamos un objeto Conexion para inicializarla y poder abrir y cerrar la conexión cuando sea necesario.
 
 	public Factura_ProductoDAO(Conexion con) {
 		super();
@@ -22,7 +22,7 @@ public class Factura_ProductoDAO {
 	}
 	
 	
-	public void insertCSV(String archivo) throws SQLException {
+	public void insertCSV(String archivo) throws SQLException { // Con este método leemos el archivo CSV, obtenemos los datos de las facturas-productos y las subimos a la base de datos.
 		String insert = "INSERT INTO factura_producto (cantidad, id_producto, id_factura) VALUES(?,?,?)";
 		Connection con = conexion.open();
 		PreparedStatement ps = con.prepareStatement(insert);
